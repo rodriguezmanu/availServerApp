37f5030cd75f8d460b2084a7717b9518064fd549
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-08-29 03:47:40 -0300

    Adding improvements to deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-preprocess');
 
   grunt
       .initConfig({
@@ -19,6 +24,62 @@ module.exports = function(grunt) {
           server : {
             url : 'http://localhost:8080'
           }
+        },
+
+        //Set env
+        env: {
+            dev: {
+                NODE_ENV: 'development'
+            },
+            prod : {
+                NODE_ENV: 'production'
+            }
+        },
+
+        // minify js
+        uglify: {
+           options: {
+             mangle: false
+           },
+           min: {
+            files: [{
+                src: 'tmp/app/**/*.js',
+                dest: 'dist/app/app.min.js'
+            }]
+           }
+         },
+
+         // copy json mocks files
+         copy: {
+           main: {
+             files: [
+               {expand: true, src: ['app/mocks/*.json'], dest: 'dist/'}
+             ],
+           },
+         },
+
+        // Compiles ES6 to JavaScript using Babel
+        babel : {
+          options : {
+            sourceMap : true,
+            presets: ['es2015']
+          },
+          dist: {
+            files: [{
+              expand: true,
+              src: [
+                'app/**/*.js',
+                '!app/**/*.spec.js'
+              ],
+              dest: 'tmp/',
+            }]
+          },
+        },
+
+        //adding conditial index.html
+        preprocess : {
+          src  : 'base.html',
+          dest : 'index.html'
         },
 
         //express connect
@@ -88,6 +149,7 @@ module.exports = function(grunt) {
     });
 
   grunt.registerTask('test',['karma']);
-  grunt.registerTask('serve', ['express:dev', 'open', 'watch']);
+  grunt.registerTask('serve', ['express:dev', 'env:dev', 'preprocess', 'open', 'watch']);
   grunt.registerTask('lint', ['jscs', 'jshint']);
+  grunt.registerTask('build', ['babel', 'uglify', 'env:prod', 'preprocess', 'copy']);
 };
